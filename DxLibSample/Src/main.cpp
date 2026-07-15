a0fd867d0e78e602807b66f523580386baff06d0
kirit <kiritoshikahara@gmail.com>
2026-07-16 03:12:37 +0900

    appの呼び出しを追加

--- DxLibSample/Src/main.cpp
+++ DxLibSample/Src/main.cpp
@@ -1,8 +1,11 @@
 #include "DxLib.h"
+#include<System/Application/Application.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-
-
+	auto* app = Application::Get();
+	app->Initialize({});
+	app->Run();
+	app->Finalize();
 	return 0;
 }
